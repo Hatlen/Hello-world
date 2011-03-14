8c875c5144c8a52c18f720242d8305e2a4d91564
Trond Hatlen <trondfroding@gmail.com>
2011-03-14 21:57:06 +0100

    And moon

--- ruby.rb
+++ ruby.rb
@@ -1,11 +1,11 @@
 class HelloWorld
 	def self.hey
-		puts 'Hello mother earth'
+		puts 'Hello mother earth, and moon'
 	end
-	def self.goodbye
-		puts 'And goodbye'
+	def self.bye
+		puts 'And goodbye, may satan be with you'
 	end
 end
 
 HelloWorld.hey
-HelloWorld.goodbye
+HelloWorld.bye
